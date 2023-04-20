d64056f4b1656e84e67e75731637406f610fb881
Corentin Beaudet <corentin.abbruzzese14@gmail.com>
2023-04-20 07:11:07 +0200

    add function close modale, return first page modale and sup galery

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -2,8 +2,8 @@ const gallery = document.querySelector('.gallery');
 const divFilters = document.querySelector('.filters');
 const banderolle = document.querySelector('#banderolle');
 const admin = document.querySelectorAll('.admin');
-const shadow = document.querySelector('.shadow');
 const modale = document.querySelector('.modale');
+const shadow = document.querySelector('.shadow');
 
 const token = sessionStorage.getItem('token');
 if (token) {
@@ -32,6 +32,7 @@ async function main() {
 
     createFigure(dataWorks, gallery);
     filters(dataCategory, dataWorks);
+
     openModale(dataWorks);
 }
 main();
@@ -53,21 +54,21 @@ function filters(dataCategory, dataWorks) {
 
     allFilters[0].addEventListener('click', () => {
         gallery.innerHTML = "";
-        createFigure(dataWorks);
+        createFigure(dataWorks, gallery);
         colorFilters(allFilters[0]);
     });
 
     for (let i = 1; i < allFilters.length; i++) {
         allFilters[i].addEventListener('click', () => {
-            const newList = dataWorks.filter(function(data) {
+            const newList = dataWorks.filter(function (data) {
                 return data.categoryId === i;
             });
             colorFilters(allFilters[i]);
             gallery.innerHTML = "";
-            createFigure(newList);
+            createFigure(newList, gallery);
         });
     }
-} 
+}
 
 function createFilters(dataCategory) {
     const btnAll = document.createElement('button');
@@ -94,47 +95,56 @@ function colorFilters(actif) {
 function openModale(dataWorks) {
     const btnModale = document.querySelector('.btnModale');
     btnModale.addEventListener('click', () => {
-        shadow.style.display = "flex";
-        modale.style.display = "flex";
-        modale.innerHTML = `
-            <span>
-                <i class="fa-solid fa-arrow-left"></i>
-                <i class="fa-solid fa-xmark"></i>
-            </span>
-            <h2>Galerie photo</h2>
-            <div class="divModale pageOne"></div>
-            <button class="addPicture">Ajouter une photo</button>
-            <button class="supGallery">Supprimer la galerie</button>
-        `;
-
-        const btnReturn = document.querySelector('.fa-arrow-left');
-        btnReturn.style.zIndex = "-2";
-
-        const divModale = document.querySelector('.divModale');
-        createFigure(dataWorks, divModale);
-
-        const figures = divModale.children;
-        for (let figure of figures) {
-            const del = document.createElement('i');
-            del.className = 'fas fa-ban';
-            figure.appendChild(del);
-        }
-
-        const btnDelete = document.querySelectorAll('.fa-ban');
-        for (let i = 0; i < btnDelete.length; i++) {
-            btnDelete[i].value = dataWorks[i].id;
-        }
-
-        const figuresModale = document.querySelectorAll('.divModale figure figcaption');
-        for (let figcaption of figuresModale) {
-            figcaption.textContent = "éditer";
-        }
-        pageTwoModale(); 
-        deleteFigure();       
+        pageOneModale(dataWorks);
     });
 }
 
-function pageTwoModale() {
+function pageOneModale(dataWorks) {
+    shadow.style.display = "flex";
+    modale.style.display = "flex";
+    modale.innerHTML = `
+        <span>
+            <i class="fa-solid fa-arrow-left"></i>
+            <i class="fa-solid fa-xmark"></i>
+        </span>
+        <h2>Galerie photo</h2>
+        <div class="divModale pageOne"></div>
+        <button class="addPicture">Ajouter une photo</button>
+        <button class="supGallery">Supprimer la galerie</button>
+    `;
+
+    const btnReturn = document.querySelector('.fa-arrow-left');
+    btnReturn.style.zIndex = "-2";
+
+    const divModale = document.querySelector('.divModale');
+    createFigure(dataWorks, divModale);
+
+    const figures = divModale.children;
+    for (let figure of figures) {
+        const del = document.createElement('i');
+        del.className = 'fas fa-ban';
+        figure.appendChild(del);
+    }
+
+    const btnDelete = document.querySelectorAll('.fa-ban');
+    for (let i = 0; i < btnDelete.length; i++) {
+        btnDelete[i].value = dataWorks[i].id;
+    }
+
+    const figuresModale = document.querySelectorAll('.divModale figure figcaption');
+    for (let figcaption of figuresModale) {
+        figcaption.textContent = "éditer";
+    }
+
+    const close = document.querySelector('.fa-xmark');
+    closeModale(shadow, dataWorks);
+    closeModale(close, dataWorks);
+    pageTwoModale(dataWorks);
+    deleteGallery();
+    deleteFigure();
+}
+
+function pageTwoModale(dataWorks) {
     const button = document.querySelector('.addPicture');
     button.addEventListener('click', () => {
         const btnReturn = document.querySelector('.fa-arrow-left');
@@ -175,6 +185,11 @@ function pageTwoModale() {
             <div class="preview"></div>
             <input type="file">
         `;
+
+        const close = document.querySelector('.fa-xmark');
+        returnPageOne(btnReturn, dataWorks);
+        closeModale(shadow, dataWorks);
+        closeModale(close, dataWorks);
         addNewFigure();
     });
 }
@@ -184,13 +199,30 @@ function deleteFigure() {
     for (let btn of allDelete) {
         btn.addEventListener('click', () => {
             const id = btn.value;
-            
+
             fetch(`http://localhost:5678/api/works/${id}`, {
                 method: "DELETE",
                 headers: { "Authorization": `Bearer ${token}` },
             });
         });
     }
+}
+
+function deleteGallery() {
+    const supGallery = document.querySelector('.supGallery');
+    const allFigure = document.querySelectorAll('.divModale .fa-ban');
+    supGallery.addEventListener('click', () => {
+        for (let value of allFigure) {
+            const id = value.value;
+            fetch(`http://localhost:5678/api/works/${id}`, {
+                method: "DELETE",
+                headers: {
+                    accept: "*/*",
+                    Authorization: `Bearer ${token}`
+                }
+            });
+        }
+    });
 }
 
 function addNewFigure() {
@@ -209,7 +241,7 @@ function addNewFigure() {
         const title = document.querySelector('.divModale input[type=text]').value;
 
         const data = new FormData();
-        data.append("image",img);
+        data.append("image", img);
         data.append("title", title);
         data.append("category", category);
 
@@ -230,8 +262,24 @@ function previewImg(imgUrl) {
 
     const url = new FileReader();
     url.readAsDataURL(imgUrl);
-    url.onload = function(url) {
+    url.onload = function (url) {
         img.src = url.target.result;
         preview.appendChild(img);
     }
+}
+
+function closeModale(btn, dataWorks) {
+    btn.addEventListener('click', () => {
+        shadow.style.display = "none";
+        modale.style.display = "none";
+        modale.innerHTML = "";
+        openModale(dataWorks);
+    });
+}
+
+function returnPageOne(btn, dataWorks) {
+    btn.addEventListener('click', () => {
+        modale.innerHTML = "";
+        pageOneModale(dataWorks);
+    });
 }
