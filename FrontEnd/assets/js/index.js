b6c7952b2a2efd3bac1336996a8963f39c95f454
Corentin Beaudet <corentin.abbruzzese14@gmail.com>
2023-04-20 03:14:54 +0200

    add function showFigure, createFilters, filters and colorFilters

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -16,4 +16,63 @@ async function getCategoryAPI() {
 async function main() {
     const dataWorks = await getWorksAPI();
     const dataCategory = await getCategoryAPI();
+
+    createFigure(dataWorks);
+    filters(dataCategory, dataWorks);
+}
+main();
+
+function createFigure(dataWorks) {
+    for (let data of dataWorks) {
+        const figure = document.createElement('figure');
+        figure.innerHTML = `
+            <img src="${data.imageUrl}">
+            <figcaption>${data.title}</figcation>
+        `;
+        gallery.appendChild(figure);
+    }
+}
+
+function filters(dataCategory, dataWorks) {
+    createFilters(dataCategory);
+    const allFilters = divFilters.children;
+
+    allFilters[0].addEventListener('click', () => {
+        gallery.innerHTML = "";
+        createFigure(dataWorks);
+        colorFilters(allFilters[0]);
+    });
+
+    for (let i = 1; i < allFilters.length; i++) {
+        allFilters[i].addEventListener('click', () => {
+            const newList = dataWorks.filter(function(data) {
+                return data.categoryId === i;
+            });
+            colorFilters(allFilters[i]);
+            gallery.innerHTML = "";
+            createFigure(newList);
+        });
+    }
+} 
+
+function createFilters(dataCategory) {
+    const btnAll = document.createElement('button');
+    btnAll.textContent = "Tous";
+    divFilters.appendChild(btnAll);
+
+    for (let category of dataCategory) {
+        const filter = document.createElement('button');
+        filter.textContent = category.name;
+        divFilters.appendChild(filter);
+    }
+}
+
+function colorFilters(actif) {
+    const allFilters = divFilters.children;
+    for (let btn of allFilters) {
+        btn.style.backgroundColor = "transparent";
+        btn.style.color = "#1D6154";
+    }
+    actif.style.backgroundColor = "#1D6154";
+    actif.style.color = "white";
 }
